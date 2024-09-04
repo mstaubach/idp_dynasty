5c1ed767e75abdc057b9702184e86f814be31359
Michael Staubach <staubach.6@icloud.com>
2024-09-03 20:36:23 -0400

    Grabbing each of the team owners ids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import fetchTeams from "./pages/api/sleeper";
+export default async function Home() {
+  const response = await fetch(
+    "https://api.sleeper.app/v1/league/1048426134855081984/rosters"
+  );
+  let teams = await response.json();
 
-fetchTeams();
-
-export default function Home() {
   return (
     <main>
-      <h1>IDP Dynasty</h1>
+      <h1>IDP</h1>
+      <ul>
+        {teams.map((team: any) => (
+          <li key={team.id}>{team.owner_id}</li>
+        ))}
+      </ul>
     </main>
   );
 }
--- src/app/pages/api/sleeper.ts
+++ src/app/pages/api/sleeper.ts
@@ -1,10 +1,11 @@
 const fetchTeams = async () => {
   try {
     const response = await fetch(
-      "https://api.sleeper.app/v1/league/1048426134855081984"
+      "https://api.sleeper.app/v1/league/1048426134855081984/rosters"
     );
     const teamData = await response.json();
     console.log(teamData);
+    return teamData;
   } catch (error) {
     console.log(error);
   }
