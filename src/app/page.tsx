b3984c063546be0b67dc929e17854d8234cd1d0d
Michael Staubach <staubach.6@icloud.com>
2024-09-04 12:34:47 -0400

    Renaming the standstable file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import StandingsTable from "./(components)/StandingsTable";
 export default async function Home() {
   return (
     <main>
-      <h1>IDP</h1>
+      <h1>IDP League Home Page</h1>
+      <div>
+        <p>About the league here.</p>
+      </div>
       <StandingsTable />
     </main>
   );
