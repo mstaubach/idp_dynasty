f70a6bc7ef5bbcc8ffbba4187a5e3e4f8afcce76
Michael Staubach <staubach.6@icloud.com>
2024-09-04 14:08:39 -0400

    Updating NavBar to include images

--- src/app/(components)/NavBar.jsx
+++ src/app/(components)/NavBar.jsx
@@ -2,19 +2,38 @@ import Link from "next/link";
 import Image from "next/image";
 
 const NavBar = () => {
+  let height = 25;
+  let width = 25;
   return (
-    <nav className="flex justify-between border-b-4 border-white p-2.5">
-      <div className="flex justify-start px-10">
-        <a href="/">
-          <Image src="" width={50} height={50} alt="Home Page" />
-        </a>
+    <nav className="border-b-4 border-white p-2.5">
+      <div className="flex justify-center p-5">
+        <h1 className="font-extrabold text-3xl">IDP Dynasty</h1>
       </div>
-      <div className="flex justify-center">
-        <h1 className="bold-text text-3xl">IDP Dynasty</h1>
-      </div>
-      <div className="flex justify-end px-10">
-        <div className="my-auto">
+      <div className="flex justify-center px-5">
+        <div className="px-10 flex justify-center">
+          <Image sr="" height={height} width={width} alt="Home Button." />
           <Link href="/">Home</Link>
+        </div>
+        <div className="px-10 flex justify-center">
+          <Image sr="" height={height} width={width} alt="Matchups Section." />
+          <Link href="/">Matchups</Link>
+        </div>
+        <div className="px-10 flex justify-center">
+          <Image
+            sr=""
+            height={height}
+            width={width}
+            alt="Transactions Section."
+          />
+          <Link href="/">Transactions</Link>
+        </div>
+        <div className="px-10 flex justify-center">
+          <Image sr="" height={height} width={width} alt="Rosters Section." />
+          <Link href="/">Rosters</Link>
+        </div>
+        <div className="px-10 flex justify-center">
+          <Image sr="" height={height} width={width} alt="Standings Section." />
+          <Link href="/">Standings</Link>
         </div>
       </div>
     </nav>
