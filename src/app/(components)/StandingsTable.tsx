b38328940779e03e5016a50a8506b2305b86c423
Michael Staubach <staubach.6@icloud.com>
2024-09-03 22:09:02 -0400

    Seperating standings table to its own components

---
+++ src/app/(components)/StandingsTable.tsx
@@ -0,0 +1,28 @@
+export default async function StandingsTable() {
+  const response = await fetch(
+    "https://api.sleeper.app/v1/league/1048426134855081984/rosters"
+  );
+  let teams = await response.json();
+  return (
+    <div>
+      <table className="border-collapse border border-white py-10">
+        <thead>
+          <tr>
+            <th className="border border-white p-4">Name</th>
+            <th className="border border-white p-4">Wins</th>
+            <th className="border border-white p-4">Losses</th>
+            <th className="border border-white p-4">Ties (Ew)</th>
+          </tr>
+        </thead>
+        {teams.map((team: any) => (
+          <tbody key="">
+            <td className="border border-white p-4">{team.roster_id}</td>
+            <td className="border border-white p-4">{team.settings.wins}</td>
+            <td className="border border-white p-4">{team.settings.losses}</td>
+            <td className="border border-white p-4">{team.settings.ties}</td>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
+}
