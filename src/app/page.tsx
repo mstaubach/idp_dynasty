710bc605b11156c5d5b1d065b923d4cf990da68a
Michael Staubach <staubach.6@icloud.com>
2024-09-03 21:50:54 -0400

    Standings table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,33 @@ export default async function Home() {
   );
   let teams = await response.json();
 
+  console.log(teams);
+
   return (
     <main>
       <h1>IDP</h1>
-      <ul>
-        {teams.map((team: any) => (
-          <li key={team.id}>{team.owner_id}</li>
-        ))}
-      </ul>
+      <div>
+        <table className="border-collapse border border-white py-10">
+          <thead>
+            <tr>
+              <th className="border border-white p-4">Name</th>
+              <th className="border border-white p-4">Wins</th>
+              <th className="border border-white p-4">Losses</th>
+              <th className="border border-white p-4">Ties (Ew)</th>
+            </tr>
+          </thead>
+          {teams.map((team: any) => (
+            <tbody key="">
+              <td className="border border-white p-4">{team.roster_id}</td>
+              <td className="border border-white p-4">{team.settings.wins}</td>
+              <td className="border border-white p-4">
+                {team.settings.losses}
+              </td>
+              <td className="border border-white p-4">{team.settings.ties}</td>
+            </tbody>
+          ))}
+        </table>
+      </div>
     </main>
   );
 }
