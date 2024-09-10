3704de960916f44550af7f7820a4bf4fb629852d
Michael Staubach <staubach.6@icloud.com>
2024-09-09 20:33:08 -0400

    Cleaning up the home page

---
+++ src/app/(components)/FirstPlaceFinish.jsx
@@ -0,0 +1,5 @@
+const FirstPlaceFinish = () => {
+  return <div>Hold for 1st Place Finish</div>;
+};
+
+export default FirstPlaceFinish;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import StandingsTable from "./(components)/StandingsTable";
+import FirstPlaceFinish from "./(components)/FirstPlaceFinish";
 
 export default async function Home() {
   return (
-    <main>
-      <h1>IDP League Home Page</h1>
-
-      <p>
-        The IDP Dynasty league originally began as a keeper league that
-        eventually transformed into a dynasty league. The individual defensive
-        format has the teams start 19 total players - offense and defense
-        combined.
-      </p>
-
-      <StandingsTable />
+    <main className="flex justify-between">
+      <div className="p-10">
+        <h1 className="p-5 font-bold">IDP League Home Page</h1>
+        <p>
+          The IDP Dynasty league originally began as a keeper league that
+          eventually transformed into a dynasty league. The individual defensive
+          format has the teams start 19 total players - offense and defense
+          combined.
+        </p>
+      </div>
+      <div className="p-10">
+        <FirstPlaceFinish />
+        <StandingsTable />
+      </div>
     </main>
   );
 }
