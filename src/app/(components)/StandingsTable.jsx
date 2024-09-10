11d64c33b341bfd81b8558968e2fd720eefc36b2
Michael Staubach <staubach.6@icloud.com>
2024-09-09 21:23:20 -0400

    Sorting wins column in StandingsTable

--- src/app/(components)/StandingsTable.jsx
+++ src/app/(components)/StandingsTable.jsx
@@ -1,8 +1,17 @@
+import { STATIC_STATUS_PAGE_GET_INITIAL_PROPS_ERROR } from "next/dist/lib/constants";
+import { requestFormReset } from "react-dom";
+
 export default async function StandingsTable() {
   const response = await fetch(
     "https://api.sleeper.app/v1/league/1048426134855081984/rosters"
   );
   let teams = await response.json();
+
+  //Sorting wins from highest to lowest
+  teams.sort(
+    (a, b) => parseFloat(a.settings.wins) - parseFloat(b.settings.wiins)
+  );
+
   return (
     <div>
       <table className="border-collapse border border-white py-10">
